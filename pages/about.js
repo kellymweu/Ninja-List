48c54aae58c436c6eecb3cb962afb97ac83f315a
Kelly MWeu <mweukelly@gmail.com>
2023-04-13 21:50:02 +0300

    stage

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div>
+      <h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec dui consequat, pellentesque elit vel, pretium risus. Nunc magna justo, faucibus quis gravida nec, pulvinar id libero</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec dui consequat, pellentesque elit vel, pretium risus. Nunc magna justo, faucibus quis gravida nec, pulvinar id libero</p>
+      </h1>
+    </div>
+  );
+};
+
+export default About;
